5bd687c6a281e6ecca2ae95db964d0175cda1027
Innokod <Osipovinn@list.ru>
2025-09-25 05:10:36 -0700

    Alpha, add StringBuilder

--- InvestmentHandler/Services/DailyMarketDataHtmlStatisticsServcice.cs
+++ InvestmentHandler/Services/DailyMarketDataHtmlStatisticsServcice.cs
@@ -1,5 +1,6 @@
 ﻿using InvestmentHandler.Enumerators;
 using InvestmentHandler.Models;
+using System.Text;
 
 namespace InvestmentHandler.Services
 {
@@ -8,56 +9,66 @@ namespace InvestmentHandler.Services
     /// </summary>
     public class DailyMarketDataHtmlStatisticsServcice : IDailyMarketDataHtmlStatisticsServcice
     {
-        /// <summary>
-        /// Translate into HTML
-        /// </summary>
-        /// <param name="datas"></param>
-        /// <returns></returns>
         public string GetDailyMarketDataPriceChangeStatistics(List<DailyMarketData> datas, DataFormatOptions dataFormatOptions)
         {
+            StringBuilder stringBuilder = new StringBuilder();
+            /// <summary>
+            /// Translate into HTML
+            /// </summary>
             if (dataFormatOptions == DataFormatOptions.HTML)
             {
-                string result = @"
-<table>
-    <thead>
-        <tr>
-            <th>Инструмент</th>
-            <th>Дата</th>
-            <th>Изменение цены</th>
-        </tr>
-    </thead>
-    <tbody>";
+                stringBuilder.AppendLine("<table>");
+                stringBuilder.AppendLine("\t<thead>");
+                stringBuilder.AppendLine("\t\t<tr>");
+                stringBuilder.AppendLine("\t\t\t<th>Инструмент</th>");
+                stringBuilder.AppendLine("\t\t\t<th>Дата</th>");
+                stringBuilder.AppendLine("\t\t\t<th>Изменение цены</th>");
+                stringBuilder.AppendLine("\t\t</tr>");
+                stringBuilder.AppendLine("\t</thead>");
+                stringBuilder.AppendLine("\t<tbody>");
                 foreach (var data in datas)
                 {
-                    result += $@"
-        <tr>
-            <td>{data.InstrumentCode}</td>
-            <td>{data.Date:dd.MM.yyyy}</td>
-            <td>Цена за этот день выросла/упала на {data.Price / 90 * 100} ({data.Price - 90}р.)(сегоднешнея стоимоть - 90р)</td>
-        </tr>";
+                    stringBuilder.AppendLine("\t\t<tr>");
+                    stringBuilder.AppendLine($"\t\t\t<td>{data.InstrumentCode}</td>");
+                    stringBuilder.AppendLine($"\t\t\t<td>{data.Date:dd.MM.yyyy}</td>");
+                    if (data.Price - 90 >= 0) 
+                    {
+                        stringBuilder.AppendLine($"\t\t\t<td>Цена за этот день выросла на {Math.Round(data.Price / 90 * 100)}% ({data.Price - 90}р.)(сегоднешнея стоимоть - 90р)</td>");
+                    }
+                    else
+                    {
+                        stringBuilder.AppendLine($"\t\t\t<td>Цена за этот день упала на {100 - Math.Round(data.Price / 90 * 100)}% ({data.Price - 90}р.)(сегоднешнея стоимоть - 90р)</td>");
+                    }
+                    stringBuilder.AppendLine("\t\t</tr>");
                 }
-                result += @"
-    </tbody>
-</table>";
-                return result;
+                stringBuilder.AppendLine("\t</tbody>");
+                stringBuilder.AppendLine("</table>");
             }
+            /// <summary>
+            /// Translate into XML
+            /// </summary>
             else
             {
-                string result = @"
-<Items>";
+                stringBuilder.AppendLine("<Items>");
                 foreach (var data in datas)
                 {
-                    result += $@"
-    <Item>
-        <instrument>{data.InstrumentCode}</instrument>
-        <date>{data.Date:dd.MM.yyyy}</date>
-        <change>{data.Price / 90 * 100 - 100:+0.0%}</change>
-    </Item>";
+                    var changeSign = data.Price - 90 >= 0 ? "+" : "-";
+                    stringBuilder.AppendLine("\t<Item>");
+                    stringBuilder.AppendLine($"\t\t<instrument>{data.InstrumentCode}</instrument>");
+                    stringBuilder.AppendLine($"\t\t<date>{data.Date:dd.MM.yyyy}</date>");
+                    if (data.Price - 90 >= 0)
+                    {
+                        stringBuilder.AppendLine($"\t\t<change>{changeSign}{Math.Round(data.Price / 90 * 100)}%</change>");
+                    }
+                    else
+                    {
+                        stringBuilder.AppendLine($"\t\t<change>{changeSign}{100 - Math.Round(data.Price / 90 * 100)}%</change>");
+                    }
+                    stringBuilder.AppendLine("\t</Item>");
                 }
-                result += @"
-</Items>";
-                return result;
+                stringBuilder.AppendLine("</Items>");
             }
+            return stringBuilder.ToString();
         }
     }
 }
--- InvestmentHandler/obj/Debug/net9.0/InvestmentHandler.AssemblyInfo.cs
+++ InvestmentHandler/obj/Debug/net9.0/InvestmentHandler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvestmentHandler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1cbcba2f6718f04efd1dc7fca251e7f493ae585")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvestmentHandler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvestmentHandler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
